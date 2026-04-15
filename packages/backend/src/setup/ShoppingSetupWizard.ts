bc8215f3817ec35fde449dc88e1a8c7a901bf68f
Jeongho Nam <samchon.github@gmail.com>
2026-04-15 18:28:26 +0900

    Fix product image URLs

--- packages/backend/src/setup/ShoppingSetupWizard.ts
+++ packages/backend/src/setup/ShoppingSetupWizard.ts
@@ -19,12 +19,9 @@ export namespace ShoppingSetupWizard {
       throw new Error(
         "Erron on SetupWizard.schema(): unable to reset database in non-test mode.",
       );
-    const execute = (type: string) => (argv: string) =>
-      cp.execSync(`npx prisma migrate ${type} --schema=prisma/schema ${argv}`, {
-        stdio: "inherit",
-      });
-    execute("reset")("--force");
-    execute("dev")("--name init");
+    cp.execSync("npx prisma db push --force-reset --schema=prisma/schema", {
+      stdio: "inherit",
+    });
   }
 
   export async function seed(): Promise<void> {
