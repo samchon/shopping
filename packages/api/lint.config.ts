e2501870327db7ee2d8e65d8b41452cfc36ae9d7
Jeongho Nam <samchon.github@gmail.com>
2026-07-10 01:02:13 +0900

    complete configuration problem

--- packages/api/lint.config.ts
+++ packages/api/lint.config.ts
@@ -1,7 +1,8 @@
 import type { ITtscLintConfig } from "@ttsc/lint";
 
 export default async () => {
-  const base = (await import("@samchon/shopping-config/lint")).default;
+  const base = (await import("@samchon/shopping-config/lint.config.ts"))
+    .default;
   return {
     ...base,
     ignores: ["src/functional/**/*.ts"],
