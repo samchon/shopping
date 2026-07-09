89801f0be765a27f7e1d977509fb44568046f267
Jeongho Nam <samchon.github@gmail.com>
2026-07-10 04:39:36 +0900

    re-setup

--- packages/backend/src/utils/CsvUtil.ts
+++ packages/backend/src/utils/CsvUtil.ts
@@ -14,22 +14,22 @@ export namespace CsvUtil {
     async (content: string): Promise<Array<{ [key in Fields]: string }>> => {
       const raw = await CsvUtil.raw(content);
 
-      const top: string[] = raw[0];
+      const top: string[] = raw[0]!;
       const indexes: Record<string, number> = Object.fromEntries(
-        fields.map((f) => [f, top.findIndex((t) => t === f)]),
+        fields.map((f) => [f, top.findIndex((t) => t === f)])
       );
       const notFound: string[] = fields.filter((f) => indexes[f] === -1);
       if (notFound.length !== 0)
         throw new Error(
           `Error on CsvParser.parse(): unable to found those fields: ${notFound.join(
-            ", ",
-          )}`,
+            ", "
+          )}`
         );
 
       return raw
         .slice(1)
         .map((row) =>
-          Object.fromEntries(fields.map((f) => [f, row[indexes[f]]])),
+          Object.fromEntries(fields.map((f) => [f, row[indexes[f]!]]))
         ) as Array<{ [key in Fields]: string }>;
     };
 }
